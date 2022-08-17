fefe6a3a0b20d69e313367882bbf10043a8681e5
jtiag <joeltiago00745@gmail.com>
2022-08-17 10:25:02 -0300

    Added changes in sls

--- src/handler.js
+++ src/handler.js
@@ -1,23 +1,11 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const app = express();
+"use strict";
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
-
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
-
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-module.exports.handler = serverless(app);
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Api Serverless ON!",
+      input: event
+    })
+  };
+}
